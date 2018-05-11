c97ab899c75b4ce764b0491a99519335cadfa443
Nestor Martinez Ostoa <nestor.martinez.98@hotmail.com>
2018-05-10 22:04:59 -0500

    update2

--- proyectoWeb_padmex/proyectoWeb_padmex/urls.py
+++ proyectoWeb_padmex/proyectoWeb_padmex/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('padmex/', include('padmex.urls')),
+    path('', include('padmex.urls')),
+    re_path(r'^login/', login_view, name='login'),
+    re_path(r'^logout/', login_view, name='logout'),
 ]
